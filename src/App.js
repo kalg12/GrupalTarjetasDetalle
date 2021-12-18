5de10346fed87e02e5bd29d1463f1e0ec4e554d4
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2021-12-18 13:20:24 -0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
 
 function App() {
   return (
+    <Router>  
     <Navbar />
+    <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer className="py-3 my-4">
+            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <li className="nav-item"><a href="/" className="nav-link px-2 text-muted">Inicio</a></li>
+            <li className="nav-item"><a href="/" className="nav-link px-2 text-muted">Nosotros</a></li>
+            <li className="nav-item"><a href="/" className="nav-link px-2 text-muted">Productos</a></li>
+            </ul>
+            <p className="text-center text-muted">© 2021 Desarrollado por Luisa Huanca, Jesús Fernández y Kevin Luciano</p>
+        </footer>
+    )
+}
