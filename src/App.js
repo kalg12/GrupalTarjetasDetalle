88f87062d1e7f9de5e7a402dd853cc85e738b155
Jesus <jesus.fdz93@gmail.com>
2021-12-18 13:37:20 -0600

    Solucion al conflict

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-<<<<<<< HEAD
 import "./style.css";
 import { Navbar } from './components/Navbar';
 import { Nosotros } from './components/Nosotros';
-=======
-import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer';
->>>>>>> 5de10346fed87e02e5bd29d1463f1e0ec4e554d4
 
 
 function App() {
 
   return (
-<<<<<<< HEAD
     <Router>
        <Navbar />
        <div className='container'>
@@ -25,12 +19,6 @@ function App() {
        </div>
     </Router>
 
-=======
-    <Router>  
-    <Navbar />
-    <Footer />
-    </Router>
->>>>>>> 5de10346fed87e02e5bd29d1463f1e0ec4e554d4
   );
   
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import {Link} from 'react-router-dom';
 
@@ -40,30 +39,3 @@ export const Navbar = () => {
       </nav>
     )
 };
-=======
-import React from 'react'
-
-export const Navbar = () => {
-    return (
-
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-                <img src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width={30} height={24} className="d-inline-block align-text-top" />
-                        Bootstrap
-            </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Inicio</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-        </nav>
-    )
-}
->>>>>>> 5de10346fed87e02e5bd29d1463f1e0ec4e554d4
