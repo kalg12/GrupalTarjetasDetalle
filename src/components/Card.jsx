ecec08f41ea46d859fb965d2aa8e88efb117d835
Jesus <jesus.fdz93@gmail.com>
2022-01-08 09:20:02 -0600

    agregados

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,12 @@ export const Card = ({ img, nombre, id, desc }) => {
     <div className="card">
       <img src={img} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">{nombre}</h5>
+        <h5 className="card-title">
+          {id} - {nombre}
+        </h5>
         <p className="card-text">{desc}</p>
         <a href="/" className="btn btn-primary">
-          Go somewhere
+          Ver pelicula por $50.00
         </a>
       </div>
     </div>
