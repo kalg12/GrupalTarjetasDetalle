268914f0004c4ddc6b103a68d15fcac4046a62dd
Jesus <jesus.fdz93@gmail.com>
2022-01-08 08:57:42 -0600

    agregados adicionales

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Carousel } from "./Carousel";
 
 export const Inicio = () => {
-    return (
-        <div>
-            Inicio
-        </div>
-    )
-}
+  const imgArray = [
+    "https://images8.alphacoders.com/106/thumb-1920-1065658.jpg",
+    "https://www.wallpapertip.com/wmimgs/35-353305_the-avengers-wallpapers-1080p-avengers-wallpaper-hd.jpg",
+    "https://www.wallpapertip.com/wmimgs/4-41662_poster-avengers-endgame-4k.jpg",
+  ];
+  return (
+    <>
+      <Carousel img={imgArray} />
+    </>
+  );
+};
